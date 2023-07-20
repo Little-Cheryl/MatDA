976a39911ccbb2ab2321e0abbf8bcc611b94ea1b
Little-Cheryl <140030557+Little-Cheryl@users.noreply.github.com>
2023-07-20 16:04:42 -0400

    Add files via upload

---
+++ bandgap/code/Cluster/adapt-RF-FMMD-bandgap.py
@@ -0,0 +1,104 @@
+import json
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPClassifier
+from matminer.featurizers.conversions import StrToComposition
+import os
+from matminer.featurizers.composition import ElementProperty
+from adapt.feature_based import fMMD
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+
+file_path = '../..data/SparseXCluster.json'
+ncluster=50
+featurefile = '../..data/bandgap_features.csv'
+
+
+if not os.path.exists(featurefile):
+    df1 = StrToComposition().featurize_dataframe(df, "formula")
+    df1.head()
+    ep_feat = ElementProperty.from_preset(preset_name="magpie")
+    df2 = ep_feat.featurize_dataframe(df1, col_id="composition")
+    df2.head()
+    excluded = ["formula", "composition", "formula", "gfa"]
+    df2.to_csv(featurefile)
+else:  #load the features
+    df2 = pd.read_csv(featurefile)
+
+y = df2['gap expt'].values
+excluded = ["formula", "composition", "formula", "gap expt"]
+X = df2.drop(excluded, axis=1)
+
+
+with open(file_path, 'r') as file:
+    cluster_ids = json.load(file)
+
+
+num_improvement = 0
+num_worse = 0
+average_improvement = 0
+average_worse = 0
+baseline_scores=[]
+adapt_scores=[]
+fails=0
+
+
+for i, cluster_id in enumerate(range(0,ncluster)):
+    target_id_list = cluster_ids[str(cluster_id)]
+
+    Xt = X.iloc[target_id_list]
+    yt = df2["gap expt"].iloc[target_id_list].values
+
+    Xs = X.drop(target_id_list)
+    ys = df2["gap expt"].drop(target_id_list).values
+
+    src_only = RandomForestRegressor(n_estimators=100, max_depth=10)
+    print(f"{i}\t",end="")
+    src_only.fit(Xs, ys)
+
+    y_pred = src_only.predict(Xt)
+    mae = mean_absolute_error(yt, y_pred)
+    print("src_only mae: ", mae, end="")
+    baseline_scores.append(mae)
+    score=mae
+
+    adapt_model = fMMD(
+        estimator=RandomForestRegressor(n_estimators=100, max_depth=10),
+        Xt=Xt, threshold='auto', kernel='linear', gamma=1.0, degree=2, coef=1.0, copy=True, verbose=1, random_state=None
+    )
+
+
+    adapt_model.fit(Xs, ys)
+    y_pred = adapt_model.predict(Xt)
+
+    nan_count = np.sum(np.isnan(y_pred))
+    if nan_count !=0:
+        print("\tadapt_model mae: ", 'NaN', end="")
+        fails+=1
+        continue
+
+    mae = mean_absolute_error(yt, y_pred)
+    print("\tadapt_model mae: ", mae, "\n", end="")
+    adapt_score = mae
+    adapt_scores.append(adapt_score)
+
+    if adapt_score < score:
+        num_improvement += 1
+        a = -(score-adapt_score)/score
+        average_improvement += a
+    elif adapt_score > score: 
+        num_worse += 1
+        w = (adapt_score-score)/score
+        average_worse += w
+
+
+print(f"average improvement: {(average_improvement/ncluster)*100} %")
+print(f"average worse: {(average_worse/ncluster)*100} %")
+print(f"baseline avg. accuracy:{sum(baseline_scores)/len(baseline_scores)}")
+print(f"adapt avg. accuracy:{sum(adapt_scores)/len(adapt_scores)}")
+print(f"num_improvement:{num_improvement}")
+print(f"num_worse:{num_worse}")
+print("base std:",np.std(baseline_scores))
+print("adapt std:",np.std(adapt_scores))
---
+++ bandgap/code/Cluster/adapt-RF-RULSIF-bandgap.py
@@ -0,0 +1,101 @@
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+import json
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPClassifier
+from matminer.featurizers.conversions import StrToComposition
+import os
+from matminer.featurizers.composition import ElementProperty
+from adapt.instance_based import RULSIF
+
+file_path = '../..data/SparseXCluster.json'
+ncluster=50
+featurefile = '../..data/bandgap_features.csv'
+
+
+if not os.path.exists(featurefile):
+    df1 = StrToComposition().featurize_dataframe(df, "formula")
+    df1.head()
+    ep_feat = ElementProperty.from_preset(preset_name="magpie")
+    df2 = ep_feat.featurize_dataframe(df1, col_id="composition")
+    df2.head()
+    excluded = ["formula", "composition", "formula", "gap expt"]
+    df2.to_csv(featurefile)
+else:  #load the features
+    df2 = pd.read_csv(featurefile)
+
+y = df2['gap expt'].values
+excluded = ["formula", "composition", "formula", "gap expt"]
+X = df2.drop(excluded, axis=1)
+
+with open(file_path, 'r') as file:
+    cluster_ids = json.load(file)
+
+num_improvement = 0
+num_worse = 0
+average_improvement = 0
+average_worse = 0
+baseline_scores=[]
+adapt_scores=[]
+fails=0
+
+
+for i, cluster_id in enumerate(range(0,ncluster)):
+    target_id_list = cluster_ids[str(cluster_id)]
+
+    Xt = X.iloc[target_id_list]
+    yt = df2["gap expt"].iloc[target_id_list].values
+
+    Xs = X.drop(target_id_list)
+    ys = df2["gap expt"].drop(target_id_list).values
+
+    src_only = RandomForestRegressor(n_estimators=100, max_depth=10)
+    print(f"{i}\t",end="")
+
+    src_only.fit(Xs, ys)
+    y_pred = src_only.predict(Xt)
+    mae = mean_absolute_error(yt, y_pred)
+
+    print("src_only mae: ", mae, end="")
+    baseline_scores.append(mae)
+    score=mae
+
+    adapt_model = RULSIF(RandomForestRegressor(n_estimators=100, max_depth=10), 
+        Xt=Xt, kernel="rbf", alpha=0.1,verbose=0,
+               lambdas=[0.1, 1., 10.], gamma=[0.1, 1., 10.], random_state=0)
+
+    adapt_model.fit(Xs, ys)
+    y_pred = adapt_model.predict(Xt)
+
+    nan_count = np.sum(np.isnan(y_pred))
+    if nan_count !=0:
+        print("\tadapt_model mae: ", 'NaN')
+        fails+=1
+        continue
+
+    mae = mean_absolute_error(yt, y_pred)
+    print("\tadapt_model mae: ", mae, end="")
+    adapt_score = mae
+    adapt_scores.append(adapt_score)
+    
+    if adapt_score < score:
+        num_improvement += 1
+        a = -(score-adapt_score)/score
+        average_improvement += a
+    elif adapt_score > score: 
+        num_worse += 1
+        w = (adapt_score-score)/score
+        average_worse += w
+
+
+print(f"average improvement: {(average_improvement/(ncluster-fails))*100} %")
+print(f"average worse: {(average_worse/(ncluster-fails))*100} %")
+print(f"baseline avg. mae:{sum(baseline_scores)/len(baseline_scores)}")
+print(f"adapt avg. mae:{sum(adapt_scores)/len(adapt_scores)}")
+print(f"num_improvement:{num_improvement}")
+print(f"num_worse:{num_worse}")
+print("base std:",np.std(baseline_scores))
+print("adapt std:",np.std(adapt_scores))
